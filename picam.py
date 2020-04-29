3a2dfa17e0c6f4e306b7a2de9add6d9a04430f51
julien <juuu9909@gmail.com>
2020-04-28 22:32:44 -0400

    menu fonctionnelle

--- picam.py
+++ picam.py
@@ -1,8 +1,11 @@
 from picamera import PiCamera
-from time import sleep
 import tkinter as tk
-import RPi.GPIO as GPIO
 import os,os.path
+import pysftp 
+
+choices = ['off', 'auto', 'sunlight','cloudy','shade','tungsten','fluorescent','incandescent','flash','horizon']
+effects = ['none','negative','solarize','sketch','denoise','emboss','oilpaint','hatch','gpen','pastel','watercolor','film','blur','saturation','colorswap','washedout','posterise','colorpoint','colorbalance','cartoon','deinterlace1','deinterlace2']
+
 
 def PrendrePhoto():
     
@@ -17,9 +20,75 @@ def PrendrePhoto():
     camera.start_preview()
     
 def EnvoitPhoto():
+    myHostname = "192.168.0.109"
+    myUsername = "pi"
+    myPassword = "patate123"
+    cnopts = pysftp.CnOpts()
+    cnopts.hostkeys = None 
+    with pysftp.Connection(host = myHostname, username=myUsername, password=myPassword,cnopts=cnopts) as sftp:
+        print("connection etablie")
+        
+        localFilePath = '/home/pi/photo/projet/'
+        remoteFilePath = '/var/www/html/gallery/'
+        sftp.put_d(localFilePath, remoteFilePath)
+        sftp.close()
+        
+#menu debut        
+        
+def menu():
+    camera.stop_preview()
+    camera.preview_fullscreen=False
+    camera.preview_window=(0,0, 540, 300)
+    camera.resolution=(1920,1080)
+    camera.start_preview()
+    window = tk.Toplevel()
+    
+    tk.Scale(window, from_=30, to=100, orient=tk.HORIZONTAL, label = "Brightness", command=UpdateBrightness).grid(row=2,column=1)
+    tk.Scale(window, from_=-100, to=100, orient=tk.HORIZONTAL, label = "Contrast", command=UpdateContrast).grid(row=2,column=2)
+    tk.Scale(window, from_=-100, to=100, orient=tk.HORIZONTAL, label = "Sharpness", command=UpdateSharpness).grid(row=2,column=3)
+    tk.Scale(window, from_=-100, to=100, orient=tk.HORIZONTAL, label = "Saturation", command=UpdateSaturation).grid(row=3,column=1)
+    
+    AWB_Var = tk.StringVar(root)
+    AWB_Var.set(choices[0]) 
+    AWB_Option = tk.OptionMenu(window, AWB_Var, *choices, command=SetAWB).grid(row=3,column=2)
+
+    EFFECT_Var = tk.StringVar(root)
+    EFFECT_Var.set(effects[0]) 
+    EFFECT_Option = tk.OptionMenu(window, EFFECT_Var, *effects, command=SetEFFECTS).grid(row=3,column=3)
+    
+    
+def UpdateBrightness(value):
+    camera.brightness = int(value)
+    
+def UpdateContrast(value):
+    camera.contrast = int(value)
+    
+def UpdateSharpness(value):
+    camera.sharpness = int(value)
+    
+def UpdateSaturation(value):
+    camera.saturation = int(value)
+
+def SetAWB(var):
+    camera.awb_mode = var
+
+def SetEFFECTS(var):
+    camera.image_effect = var
+
+#def Zoom(var):
+#    x = float("0."+var)
+#    camera.zoom = (0.5,0.5,x,x)    
+    
+    
+    
+    
+    
+    
+#menu fin    
     
     
 def CameraON():
+    camera.stop_preview()
     camera.preview_fullscreen=False
     camera.preview_window=(-7,-100, 1080, 600)
     camera.resolution=(1920,1080)
@@ -47,21 +116,19 @@ root.title("Camera Button Test")
 root.buttonframe = tk.Frame(root)
 root.buttonframe.grid(row=5, column=3, columnspan=2)
 
+
+
 tk.Button(root.buttonframe, text='Start Camera', command=CameraON).grid(row=1, column = 1)
 tk.Button(root.buttonframe, text='Kill Camera', command=CameraOFF).grid(row=1, column = 2)
 tk.Button(root.buttonframe, text='Exit Program', command=EXIT).grid(row=1, column = 3)
 tk.Button(root.buttonframe, text='PrendrePhoto', command=PrendrePhoto).grid(row=1, column = 4)
+tk.Button(root.buttonframe, text='EnvoitPhoto', command=EnvoitPhoto).grid(row=1, column = 5)
+tk.Button(root.buttonframe, text='Menu', command=menu).grid(row=1, column = 6)
+
+
+
 
 #root.overrideredirect(True)
 
 root.mainloop()
 
-#camera.start_preview()  # Affiche la vision de la caméra
-#sleep(5)       # Attend 5 seconde
-#while True:
-    #index = index + 1
-    #repository = '/home/pi/Desktop/' + str(index) + '.jpg'
-    #print(repository)
-    #camera.capture(repository)
-#camera.stop_preview()   # Ferme la vision de la caméra
-#sleep(0.1)
